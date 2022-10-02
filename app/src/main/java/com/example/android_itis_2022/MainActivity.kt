ee1c69ab313fc221a00794eef04b2964e9086710
timur200362 <94241976+timur200362@users.noreply.github.com>
2022-10-02 16:36:18 +0300

    dz1

--- app/src/main/java/com/example/android_itis_2022/MainActivity.kt
+++ app/src/main/java/com/example/android_itis_2022/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.android_itis_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import com.example.android_itis_2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getSupportActionBar()?.hide();
+        binding=ActivityMainBinding.inflate(layoutInflater).also{
+            setContentView(it.root)
+        }
+        val binding=binding!!
+        with(binding)
+        {
+            button.setOnClickListener{
+                imageView2.visibility=if (imageView2.visibility==View.GONE){
+                    View.VISIBLE
+                } else{
+                    View.GONE
+                }
+            }
+        }
     }
 }
