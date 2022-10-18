5bf63305cecf1c78e6b06f00eb40d4cd7de1e289
timur200362 <94241976+timur200362@users.noreply.github.com>
2022-10-18 10:20:40 +0300

    dz2(zakomitil nenyznii setcontentview)

--- app/src/main/java/com/example/android_itis_2022/MainActivity.kt
+++ app/src/main/java/com/example/android_itis_2022/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
