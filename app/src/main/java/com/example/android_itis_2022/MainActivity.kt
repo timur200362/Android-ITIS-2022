464bce1efa947a690689e35f3136b8e92bb1aa06
timur200362 <94241976+timur200362@users.noreply.github.com>
2022-12-03 11:16:48 +0300

    dz6(only bottomNavigation)

--- app/src/main/java/com/example/android_itis_2022/MainActivity.kt
+++ app/src/main/java/com/example/android_itis_2022/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.android_itis_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.android_itis_2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var controller:NavController
+    private var binding:ActivityMainBinding?=null
+
     override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater).also{
+            setContentView(it.root)
+        }
+        controller=(supportFragmentManager.findFragmentById(R.id.container) as NavHostController).navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.servicesFragment2),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+//        binding?.run {
+//            bnvMain.setupWithNavController(controller)
+//        }
     }
 }
