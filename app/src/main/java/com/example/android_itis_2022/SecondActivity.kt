4178a14e32a2f7bb3f5afebf334f07bbb8473b68
timur200362 <94241976+timur200362@users.noreply.github.com>
2022-12-12 16:22:42 +0300

    dz7(design)

---
+++ app/src/main/java/com/example/android_itis_2022/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_itis_2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
