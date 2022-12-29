b0f0209e8a30ad8bb809dc092e416b4250c9ff3e
timur200362 <94241976+timur200362@users.noreply.github.com>
2022-12-29 20:31:19 +0300

    dz8(пока что без пермишенов)

--- app/src/main/java/com/example/android_itis_2022/MainActivity.kt
+++ app/src/main/java/com/example/android_itis_2022/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.android_itis_2022
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_itis_2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val getContent = registerForActivityResult(ResultActivityContract(this@MainActivity)) { bitmap: Bitmap? ->
+        binding?.imvImage?.setImageBitmap(bitmap)
+    }
+
+//    val getContentCamera = registerForActivityResult(CameraActivityResultContract()) { bitmap: Bitmap? ->
+//        binding?.imvImage?.setImageBitmap(bitmap)
+//    }
+
+    private var binding:ActivityMainBinding?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding= ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+
+            it.btnGetPicture.setOnClickListener {
+                getContent.launch("")
+            }
+        }
     }
 }
